d71aa74d0158a0be527e721fbc89298109a6fd75
Ichson <xson2582002@gmail.com>
2024-05-10 23:35:48 +0700

    4th commit

--- src/models/user.js
+++ src/models/user.js
@@ -15,14 +15,15 @@ module.exports = (sequelize, DataTypes) => {
   };
   User.init({
     email: DataTypes.STRING,
-    password:DataTypes.STRING,
+    password: DataTypes.STRING,
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
     address: DataTypes.STRING,
+    phonenumber: DataTypes.STRING,
     gender: DataTypes.BOOLEAN,
-    typeRole: DataTypes.STRING,
-    keyRole:DataTypes.STRING
-
+    image: DataTypes.STRING,
+    roleId: DataTypes.STRING,
+    positionId: DataTypes.STRING,
   }, {
     sequelize,
     modelName: 'User',
