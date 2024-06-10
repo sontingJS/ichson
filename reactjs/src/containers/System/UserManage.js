9a0a25b4fb4be5345f1a01ffdd8092e737be40ba
Ichson <xson2582002@gmail.com>
2024-06-10 16:53:57 +0700

    read data by reactjs

--- reactjs/src/containers/System/UserManage.js
+++ reactjs/src/containers/System/UserManage.js
@@ -1,20 +1,65 @@
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
+import './UserManage.scss';
+import { getAllUsers } from '../../services/userService';
 class UserManage extends Component {
-
-    state = {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            arrUser: []
+        }
     }
-
-    componentDidMount() {
-
+    async componentDidMount() {
+        let response = await getAllUsers('ALL');
+        if (response && response.errCode === 0) {
+            this.setState({
+                arrUser: response.users
+            }, () => {
+                console.log('check state user 1', this.state.arrUser)
+            })
+        }
     }
 
 
     render() {
+        console.log('check render', this.state)
+        let arrUser = this.state.arrUser;
         return (
-            <div className="text-center">Manage users</div>
+            <div className="user-container">
+                <div className='tittle text-center'>Manage users with Ich Son</div>
+                <div className='user-table mt-3 mx-1'>
+                    <table id="customers">
+                        <tr>
+                            <th>Email</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Address</th>
+                            <th>actions</th>
+                        </tr>
+                        {
+                            arrUser.map((item, index) => {
+                                console.log('Ich Son check map', item, index)
+                                return (
+                                    <>
+                                        <tr>
+                                            <td>{item.email}</td>
+                                            <td>{item.firstName}</td>
+                                            <td>{item.lastName}</td>
+                                            <td>{item.address}</td>
+                                            <td>
+                                                <button className='btn-edit'><i className="fas fa-edit"></i></button>
+                                                <button className='btn-delete'><i className="fas fa-trash-alt"></i></button>
+                                            </td>
+                                        </tr>
+                                    </>
+                                )
+                            })
+                        }
+                    </table>
+
+                </div>
+            </div>
         );
     }
 
--- reactjs/src/services/userService.js
+++ reactjs/src/services/userService.js
@@ -1,10 +1,13 @@
 import axios from "../axios";
 
-const handleLoginApi = async (userEmail, userPassword, ) => {
+const handleLoginApi = async (userEmail, userPassword,) => {
     return await axios.post('/api/login', {
         email: userEmail,
         password: userPassword
 
     })
 }
-export { handleLoginApi }
+const getAllUsers = (inputId) => {
+    return axios.get(`/api/get-all-user?id=${inputId}`)
+}
+export { handleLoginApi, getAllUsers }
